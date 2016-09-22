aa34191806f7aa0f8f0f360d22ea59d29b71b4d1
AnilDeshpande <anildesh82@gmail.com>
2016-09-22 22:50:03 +0530

    Sample Demo code dynamic broadcast receiver demo for code tutor

--- app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MainActivity.java
+++ app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MainActivity.java
@@ -1,19 +1,42 @@
 package broadcast.codetutor.youtube.edu.samplebrtutorialcode;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by anildeshpande on 12/09/16.
  */
 public class MainActivity extends AppCompatActivity {
 
-    private int MY_PERMISSION;
+    private BroadcastReceiver broadcastReceiver;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_activty);
+        broadcastReceiver=new MyBroadcastReceiver();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter=new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(broadcastReceiver,intentFilter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+
     }
 }
--- app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MyBroadcastReceiver.java
+++ app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MyBroadcastReceiver.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
  */
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context,"Broadcast Receiver Triggered",Toast.LENGTH_SHORT).show();
