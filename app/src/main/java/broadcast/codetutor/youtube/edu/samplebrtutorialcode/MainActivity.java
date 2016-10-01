c75746e619e2758b6e5a8b17ec4c57bcac514ed6
AnilDeshpande <anildesh82@gmail.com>
2016-10-01 09:34:41 +0530

    CustomIntentBroadcast Branch commit

--- app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MainActivity.java
+++ app/src/main/java/broadcast/codetutor/youtube/edu/samplebrtutorialcode/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,28 +16,48 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private BroadcastReceiver broadcastReceiver;
+    private Button buttonBroadcastIntent;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main_activty);
+        buttonBroadcastIntent=(Button)findViewById(R.id.buttonBroadcastIntent);
         broadcastReceiver=new MyBroadcastReceiver();
+
+        buttonBroadcastIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setAction("my.custom.action.tag.fordemo");
+                sendBroadcast(intent);
+            }
+        });
+
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        IntentFilter intentFilter=new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
-        registerReceiver(broadcastReceiver,intentFilter);
+        /*IntentFilter intentFilter=new IntentFilter("my.custom.action.tag.fordemo");
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        registerReceiver(broadcastReceiver,intentFilter);*/
 
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        unregisterReceiver(broadcastReceiver);
+       // unregisterReceiver(broadcastReceiver);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+       // unregisterReceiver(broadcastReceiver);
     }
 }
